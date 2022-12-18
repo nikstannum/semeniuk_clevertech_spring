063894ec2e6c60c28fedfefb601f3766070c3003
Nikita Semeniuk <nikstannum@yandex.ru>
2022-12-19 00:02:44 +0300

    logging - OK

--- src/main/java/by/clevertech/exception/RestExceptionAdvice.java
+++ src/main/java/by/clevertech/exception/RestExceptionAdvice.java
@@ -1,17 +1,16 @@
 package by.clevertech.exception;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import by.clevertech.service.dto.ErrorDto;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RestControllerAdvice(basePackages = { "by.clevertech.rest", "by.clevertech.repository", "by.clevertech.service" })
 public class RestExceptionAdvice {
-    private static final Logger log = LogManager.getLogger(RestExceptionAdvice.class);
 
     private static final String MSG_SERVER_ERROR = "Server error";
     private static final String MSG_CLIENT_ERROR = "Client error";
